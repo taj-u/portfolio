d2c91ff49e9c617cfb098dcefe851cf0d2c3364e
taj-u-LW <tajcsebsmrstu@gmail.com>
2024-04-13 16:01:14 +0600

    Add My Services section/component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Navbar from './components/Navbar/Navbar';
 import Hero from './components/Hero/Hero';
 import About from './components/About/About';
+import Services from './components/Services/Services';
 
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <Navbar/>
       <Hero/>
       <About/>
+      <Services/>
     </div>
   );
 }
